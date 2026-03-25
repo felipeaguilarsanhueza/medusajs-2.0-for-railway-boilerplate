ba51e17732f06c873dab3ac9bd552f99985a5ec3
felipeaguilarsanhueza <felipeaguilarsanhueza@gmail.com>
2026-03-25 16:04:29 -0300

    correciones mp4

--- backend/src/modules/payment-mercadopago/index.ts
+++ backend/src/modules/payment-mercadopago/index.ts
@@ -1,8 +1,5 @@
-import { ModuleProvider, Modules } from "@medusajs/framework/utils"
 import MercadoPagoProviderService from "./service"
 
-const services = [MercadoPagoProviderService]
-
-export default ModuleProvider(Modules.PAYMENT, {
-  services,
-})
+export default {
+  services: [MercadoPagoProviderService],
+}
